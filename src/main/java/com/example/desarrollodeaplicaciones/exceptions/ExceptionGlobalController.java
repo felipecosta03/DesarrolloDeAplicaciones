5d65be66ae8edd4abd7727a4719d147dc86e6883
Felipe <felipe.costa@mercadolibre.com>
2024-03-16 22:07:41 -0300

    Added error code

--- src/main/java/com/example/desarrollodeaplicaciones/exceptions/ExceptionGlobalController.java
+++ src/main/java/com/example/desarrollodeaplicaciones/exceptions/ExceptionGlobalController.java
@@ -1,13 +1,22 @@
 package com.example.desarrollodeaplicaciones.exceptions;
 
+import com.example.desarrollodeaplicaciones.dtos.ErrorCode;
 import com.example.desarrollodeaplicaciones.dtos.ErrorMessageDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice public class ExceptionGlobalController {
-    @ExceptionHandler
-    public ResponseEntity<ErrorMessageDTO> userNotFoundException(UserNotFoundException exception) {
-        return ResponseEntity.status(404).body(ErrorMessageDTO.builder().message(exception.getMessage()).status(404).build());
-    }
+@ControllerAdvice
+public class ExceptionGlobalController {
+  @ExceptionHandler
+  public ResponseEntity<ErrorMessageDTO> userNotFoundException(UserNotFoundException exception) {
+    ErrorCode errorCode = ErrorCode.NOT_FOUND;
+    return ResponseEntity.status(errorCode.getStatus())
+        .body(
+            ErrorMessageDTO.builder()
+                .message(exception.getMessage())
+                .status(errorCode.getStatus())
+                .code(errorCode)
+                .build());
+  }
 }
