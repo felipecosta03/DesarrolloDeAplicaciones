ce9cea16c90b5be03a9f3aaaa4d96be0db8d0f5b
Felipe Costa <felipe.costa@mercadolibre.com>
2024-05-15 23:59:13 -0300

    Added ping route

---
+++ src/main/java/com/example/desarrollodeaplicaciones/router/PingRouter.java
@@ -0,0 +1,13 @@
+package com.example.desarrollodeaplicaciones.router;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingRouter {
+
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+}
