ab69fd238ea7202f76a8638005481070bd285015
Felipe Costa <felipe.costa@mercadolibre.com>
2024-06-10 18:54:09 -0300

    Removed authorization for ping endpoint

--- src/main/java/com/example/desarrollodeaplicaciones/configs/SecurityConfig.java
+++ src/main/java/com/example/desarrollodeaplicaciones/configs/SecurityConfig.java
@@ -13,28 +13,29 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @Configuration
 public class SecurityConfig {
 
-  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-  public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-  }
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(
-            authRequest ->
-                authRequest
-                    .requestMatchers("/api/v1/auth/**")
-                    .permitAll()
-                    .requestMatchers("/api-docs.yaml")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated())
-        .sessionManagement(
-            sessionManagement ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
-        .build();
-  }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authRequest ->
+                                authRequest
+                                        .requestMatchers("/api/v1/auth/**")
+                                        .permitAll()
+                                        .requestMatchers("/api-docs.yaml")
+                                        .permitAll()
+                                        .requestMatchers("/ping").permitAll()
+                                        .anyRequest()
+                                        .authenticated())
+                .sessionManagement(
+                        sessionManagement ->
+                                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
+                .build();
+    }
 }
